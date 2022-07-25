1bfa5e28bb69a297394471c90e0bfd0bd078df6e
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-25 21:24:58 +0100

    Pass test for calculating account balance

--- spec/bankAccount.test.js
+++ spec/bankAccount.test.js
@@ -1,14 +1,14 @@
 const bankAccount = require("../lib/bankAccount");
 
 describe("bankAccount", () => {
-  it("it should display the column names of the bank statement", () => {
+  it("should display the column names of the bank statement", () => {
     const account = new bankAccount();
     expect(account.printStatement()).toContain(
       "date || credit || debit || balance"
     );
   });
 
-  it("it should display the column names and their values in the bank statement", () => {
+  it("should display the column names and their values in the bank statement", () => {
     const account = new bankAccount();
     account.transactionDate(0);
     account.addDeposit(0);
@@ -19,14 +19,25 @@ describe("bankAccount", () => {
     );
   });
 
-  it("it should display the column names and the date value in the bank statement", () => {
+  it("should display the column names and the date value in the bank statement", () => {
     const account = new bankAccount();
     account.transactionDate("14/01/2023");
     account.addDeposit(0);
     account.addWithdrawal(0);
     account.calculateBalance();
-    expect(account.printStatement()).toContain(
+    expect(account.printStatement()).toEqual(
       "date || credit || debit || balance\n14/01/2023 || 0 || 0 || 0"
+    );
+  });
+
+  it("displays the balance as a difference between the credit and debit", () => {
+    const account = new bankAccount();
+    account.transactionDate("14/01/2023");
+    account.addDeposit(1000);
+    account.addWithdrawal(500);
+    account.calculateBalance();
+    expect(account.printStatement()).toEqual(
+      "date || credit || debit || balance\n14/01/2023 || 1000 || 500 || 500"
     );
   });
 });
