96cc9162cdc21459d0cdb87c4170085c9b2fad00
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-25 19:40:04 +0100

    Add variable for new instance of class in the test suite

--- spec/bankAccount.test.js
+++ spec/bankAccount.test.js
@@ -1,21 +1,32 @@
 const bankAccount = require("../lib/bankAccount");
 
 describe("bankAccount", () => {
-  const account = new bankAccount();
-
   it("it should display the column names of the bank statement", () => {
+    const account = new bankAccount();
     expect(account.printStatement()).toContain(
       "date || credit || debit || balance"
     );
   });
 
   it("it should display the column names and their values in the bank statement", () => {
+    const account = new bankAccount();
     account.transactionDate(0);
     account.addDeposit(0);
     account.addWithdrawal(0);
     account.calculateBalance();
-    expect(account.printStatement()).toContain(
+    expect(account.printStatement()).toEqual(
       "date || credit || debit || balance\n0 || 0 || 0 || 0"
+    );
+  });
+
+  it("it should display the column names and the date value in the bank statement", () => {
+    const account = new bankAccount();
+    account.transactionDate("14/01/2023");
+    account.addDeposit(0);
+    account.addWithdrawal(0);
+    account.calculateBalance();
+    expect(account.printStatement()).toContain(
+      "date || credit || debit || balance\n14/01/2023 || 0 || 0 || 0"
     );
   });
 });
