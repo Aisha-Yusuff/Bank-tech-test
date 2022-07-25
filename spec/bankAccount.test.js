ccc72c22d940d98f0700ddf6cf14d00cc3edb98d
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-25 19:05:12 +0100

    Add date, deposit, withdrawals and balance methods to bankAccount class
    Add transactionDate method with date arguement, addDeposit method with amount arguement, add addWithdrawal method with amount arguement, add calculateDeposit method to bankAccount class and passed jest test

--- spec/bankAccount.test.js
+++ spec/bankAccount.test.js
@@ -4,8 +4,18 @@ describe("bankAccount", () => {
   const account = new bankAccount();
 
   it("it should display the column names of the bank statement", () => {
-    expect(account.printStatement()).toEqual(
+    expect(account.printStatement()).toContain(
       "date || credit || debit || balance"
+    );
+  });
+
+  it("it should display the column names and their values in the bank statement", () => {
+    account.transactionDate(0);
+    account.addDeposit(0);
+    account.addWithdrawal(0);
+    account.calculateBalance();
+    expect(account.printStatement()).toContain(
+      "date || credit || debit || balance\n0 || 0 || 0 || 0"
     );
   });
 });
