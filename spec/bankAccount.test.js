b375998b559cef32174a82276f03003595c798c8
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-26 15:16:08 +0100

    Update bankAccount class to display multiple transactions and pass tests

--- spec/bankAccount.test.js
+++ spec/bankAccount.test.js
@@ -55,4 +55,18 @@ describe("bankAccount", () => {
       "date || credit || debit || balance\n14/01/2023 || 500 ||  || 500"
     );
   });
+
+  it("displays a bank statement for multiple transactions", () => {
+    const account = new bankAccount();
+    account.transactionDate("10/01/2023");
+    account.addDeposit(1000);
+    account.calculateBalance();
+
+    account.transactionDate("13/01/2023");
+    account.addDeposit(2000);
+    account.calculateBalance();
+    expect(account.printStatement()).toEqual(
+      "date || credit || debit || balance\n13/01/2023 || 2000 ||  || 3000\n10/01/2023 || 1000 ||  || 1000"
+    );
+  });
 });
