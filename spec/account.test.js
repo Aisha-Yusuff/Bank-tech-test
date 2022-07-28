3c5e833516799405f5ac2bc2dc0626e3611263a2
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-28 12:29:54 +0100

    Create integration test for account and transaction class

---
+++ spec/account.test.js
@@ -0,0 +1,24 @@
+const Account = require("../lib/account");
+const Transaction = require("../lib/transaction");
+
+describe("Account", () => {
+  it("displays a statement for multiple dates, deposits and a withdrawal", () => {
+    const bankAccount = new Account();
+    bankAccount.addTransaction(new Transaction("10/01/2023", 1000, "deposit"));
+    bankAccount.addTransaction(new Transaction("13/01/2023", 2000, "deposit"));
+    bankAccount.addTransaction(
+      new Transaction("14/01/2023", 500, "withdrawal")
+    );
+    bankAccount.calculateBalance();
+    bankAccount.createStatementRows();
+    expect(bankAccount.printStatement()).toEqual(
+      "date || credit || debit || balance" +
+        "\n" +
+        "14/01/2023 || || 500.00 || 2500.00" +
+        "\n" +
+        "13/01/2023 || 2000.00 || || 3000.00" +
+        "\n" +
+        "10/01/2023 || 1000.00 || || 1000.00"
+    );
+  });
+});
